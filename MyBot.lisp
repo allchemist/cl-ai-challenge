d3ec2a981623fb2a55bb410fc727011381411348
Ivan Khokhlov <hohlovivan@gmail.com>
2010-09-21 10:47:35 +0400

    MyBot example

--- MyBot.lisp
+++ MyBot.lisp
@@ -1,10 +1,7 @@
-(require :asdf)
-(load "planet-wars/planet-wars.asd")
-(load "split-sequence/split-sequence.asd")
-(require :split-sequence)
-(require :planet-wars)
+(load "split-sequence")
+(load "planet-wars")
 
-(in-package :planet-wars)
+(in-package :pwbot)
 
 ;; utils
 
@@ -38,72 +35,31 @@
 (defun scal (list const)
   (mapcar #'(lambda (l) (* l const)) list))
 
-;; aiming
+(defun play (&key (input *standard-input*) (output *standard-output*))
+  (loop while (peek-char nil input nil nil)
+	for turn from 1
+	do (let ((game (read-game input)))
+	     (write-orders (compute-orders game) output)
+	     (write-line "go" output))))
 
-(defparameter *dist-coeff* 1.0)
-(defparameter *growth-coeff* 1.0)
-(defparameter *easy-coeff* 1.0)
-(defparameter *choose-of* 10)
 
-(defun define-aim (planet game)
-  (let* ((neutrals (neutral-planets game))
-	 (enemies (enemy-planets game))
-	 (planets (append neutrals enemies))
-	 (dist-rating
-	  (scal (normalize (mapcar #'(lambda (p) (/ (planet-distance planet p))) planets))
-		*dist-coeff*))
-	 (growth-rating
-	  (scal (normalize (mapcar #'(lambda (p) (growth p)) planets))
-		*growth-coeff*))
-	 (easy-rating
-	  (scal (normalize (mapcar #'(lambda (p) (/ (1+ (n-ships p)))) planets))
-		*easy-coeff*))
-	 (result-rating (mapcar #'(lambda (d g e) (+ d g e)) dist-rating growth-rating easy-rating))
-	 (sorted-rating (stable-sort (copy-list result-rating) #'>)))
-    (let ((dest (elt planets (position (first sorted-rating) result-rating))))
-      (cond ((member dest neutrals :test #'equalp)
-	     (when (> (n-ships planet) (+ (n-ships dest) 2))
-	       (list dest (1+ (n-ships dest)))))
-	    ((member dest enemies :test #'equalp)
-	     (when (> (n-ships planet) (* 10 (growth planet)))
-	       (list dest (floor (/ (n-ships planet) 3)))))))))
+;; here the bot code should be
 
-;; orders
-
+; zero order
 (defun compute-orders (game)
-  (let* ((source (random-elt (own-planets game)))	 
-	 (others (append (neutral-planets game) (enemy-planets game)))
-	 (dest (when (> (length others) 0) (define-aim source game))))
-    (when (and source dest)
-      (list
-       (make-order :source source
-		   :destination (first dest)
-		   :n-ships (second dest))))))
+  (declare (ignore game))
+;
+;  (list
+;   (make-order :source source
+;	       :destination dest
+;	       :n-ships n-ships))))))
+)
 
-#|
-(defun compute-orders (game)
-  (when (and (own-planets game) (enemy-planets game))
-    (remove nil (mapcar #'(lambda (p)
-			    (let ((aim (define-aim p game)))
-			      (make-order :source p
-					  :destination (first aim)
-					  :n-ships (1- (second aim)))))
-			(own-planets game)))))
-|#
 
-#|
-(sb-ext:save-lisp-and-die "MyBot"
-  :toplevel #'play
-  :save-runtime-options t
-  :executable t)
-|#
+;; here the bot code ends
 
-(defpackage :pwbot
-    (:use #:planet-wars #:common-lisp)
-  (:export #:main))
-
-(in-package :pwbot)
+;; main
 
 (defun main ()
   (let ((*random-state* (make-random-state t)))
-    (planet-wars::play)))
+    (play)))
